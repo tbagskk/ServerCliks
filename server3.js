04dc831628e0cee20fd3f98751d6ad9c3db0550e
tbagskk <tbag.sk@gmail.com>
2023-11-26 11:42:04 +0100

    deployement test

---
+++ server3.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "@colyseus/core";
+import { WebSocketTransport } from "@colyseus/ws-transport"
+import GameRoom from "./GameRoom.js";
+
+const app = express();
+const server = createServer(app); // create the http server manually
+
+const gameServer = new Server({
+  transport: new WebSocketTransport({
+    server // provide the custom server for `WebSocketTransport`
+  })
+});
+
+gameServer.define("game", GameRoom);
+
+const PORT = 3001; // spécifiez le port que vous souhaitez utiliser
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
